220ff75f3747b90847bbac5affdcefdbeb044f50
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-06 00:14:04 +0300

    Worked on a shopping app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isLightTheme, setisLightTheme] = useState(false);
+  const appClass = isLightTheme ? "App dark" : "App light";
+  function themeToggle() {
+    setisLightTheme((isLightTheme) => !isLightTheme);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={themeToggle}>
+          {isLightTheme ? "Dark Mode" : "Light Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false);
+  const liClassName = isAdded ? "in-cart" : "";
+  const buttonClassName = isAdded ? "remove" : "add";
+  function manageCart() {
+    setIsAdded((isAdded) => !isAdded);
+  }
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClassName} onClick={manageCart}>
+        {isAdded ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
